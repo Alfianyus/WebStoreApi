70118ae29199d8807ec722d0c96da5ff5e78d22a
alfian.yuska <alfian.yuska@phiraka.com>
2023-05-31 15:11:20 +0700

    editUserDto.cs

--- WebStoreApi/Models/UserDto.cs
+++ WebStoreApi/Models/UserDto.cs
@@ -1,15 +1,22 @@
-﻿namespace WebStoreApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStoreApi.Models
 {
     public class UserDto
     {
+        [Required]
         public string FirstName { get; set; } = "";
 
+        [Required(ErrorMessage ="Please provider your LastName")]
         public string LastName { get; set; }= "";
-
+        [Required ,EmailAddress]
         public string Email { get; set; } = "";
 
         public string Phone { get; set; } = "";
 
+        [Required]
+        [MinLength(5, ErrorMessage ="The Address should be at least 5 characters")]
+        [MaxLength(1000, ErrorMessage ="The Address should be less than 1000 characters")]
         public string Address { get; set; } = "";
     }
 }
